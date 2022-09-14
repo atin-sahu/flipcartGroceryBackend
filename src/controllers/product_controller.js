f45a6fe6e1d35a216198e797428ea3438f5afc66
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-09-14 22:44:10 +0530

    pagination try

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 router.get("/", async(req,res)=>{
     try {
-        const products = await Product.find().lean().exec();
+        // const page = req.query.page || 1;
+        // const pagesize = req.query.pagesize || 5;
+        // const skip = (page - 1) * pagesize;
+        // const products = await Product.find().skip(skip).limit(pagesize).lean().exec();
+        // const totalPages = Math.ceil((await Product.find().countDocuments()) / pagesize);
+        // return res.status(200).send({products, totalPages});
+        let sortvalue = Number(req.query.sort);
+        const products = await Product.find().sort({price:sortvalue}).lean().exec();
         return res.status(200).send(products);
     } catch (error) {
         return res.status(500).send({message:error.message})
