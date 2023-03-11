a6a9b86e33361dbf4597f6a5d5b99dc5419300a1
atin-sahu <jayshiv7171@gmail.com>
2023-03-11 11:50:38 +0530

    dev setup done

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 const connect = () =>{
-    return mongoose.connect("mongodb+srv://atin-sahu:atinsahu@cluster0.5qorbgq.mongodb.net/flipcart?retryWrites=true&w=majority")
+    return mongoose.connect(process.env.MONGO_URL)
 };
 module.exports = connect;
--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const connect = require("./configs/db")
 
 require("dotenv").config();
 
-app.listen(process.env.PORT || 5000, async() =>{
+app.listen(process.env.PORT || 3001, async() =>{
     try {
         await connect();
+        console.log(`listning on port ${process.env.PORT}`);
     } catch (error) {
         console.log(error);
     }
-    console.log("listning on port 5000");
 })
